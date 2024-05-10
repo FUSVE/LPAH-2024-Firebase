acfc73f59ab1c0e0404d043a09ac0d1d0c588d8d
Tassio Sirqueira <tassio@tassio.eti.br>
2024-05-09 21:30:32 -0300

    Correção
    Correção

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,19 +1,15 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore/lite';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAIghHY_F7AOh_EZwGFTySwfSUDuBCD2C4",
-    authDomain: "aula-37149.firebaseapp.com",
-    projectId: "aula-37149",
-    storageBucket: "aula-37149.appspot.com",
-    messagingSenderId: "729987422452",
-    appId: "1:729987422452:web:d1df75f811af17b36926a1"
-  };
+  apiKey: "AIzaSyAIghHY_F7AOh_EZwGFTySwfSUDuBCD2C4",
+  authDomain: "aula-37149.firebaseapp.com",
+  projectId: "aula-37149",
+  storageBucket: "aula-37149.appspot.com",
+  messagingSenderId: "729987422452",
+  appId: "1:729987422452:web:a6aff047016dad2c6926a1"
+};
 
-if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-}
-
-const db = firebase.firestore();
-
-export default db;
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
